e558806a9920162374eea81e7070ae76299b3fa8
nileshsachdeva <nileshsachdeva>
2025-08-13 15:08:54 +0000

    update app.py

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ import os
 def main():
     st.markdown("<h1>Protein Sequence Analysis App</h1><br>", unsafe_allow_html=True)
     # st.markdown("<p style='text-align: right; color: #FF4B4B;'>by Abhinav Rana</p>", unsafe_allow_html=True)
+
+
+if __name__ == "__main__":
+    main()
